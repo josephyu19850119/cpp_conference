50c4a856cdf425a50ea7931653156c7c652d2099
DESKTOP-VNPAGFS\俞悦 <yuyue@imoran.net>
2019-03-15 17:26:41 +0800

    完备shared_mutex_demo.cpp

--- shared_mutex_demo/shared_mutex_demo.cpp
+++ shared_mutex_demo/shared_mutex_demo.cpp
@@ -1,7 +1,8 @@
+#include<thread>
 #include<iostream>
 // #include"res_mgr.h"
 // #include"res_mgr_2.h"
-#include"res_mgr_3.h"
+#include"res_mgr_4.h"
 
 using namespace std;
 
@@ -9,27 +10,25 @@ using namespace std;
 
 int main()
 {
-    // shared_timed_mutex locker;
-    // thread t1([&locker]{
-    //     locker.lock();
-    //     cout<<"thread1 started"<<endl;
-    //     cout<<"thread1 done"<<endl;
-    //     locker.unlock();
-    //     });
-    // thread t2([&locker]{
-    //     locker.lock();
-    //     cout<<"thread2 started"<<endl;
-    //     cout<<"thread2 done"<<endl;
-    //     locker.unlock();
-    //     });
-
-    // locker.lock();
-    //     locker.unlock();
-    // shared_lock<shared_timed_mutex> l(locker);
-
-    // t1.join();
-    // t2.join();
-    // cout<<"done"<<endl;
     res_mgr rm;
+    rm.Load(666);
+    thread t1([&rm]{
+        for (int i = 0; i < 1000; ++i)
+        {
+            rm.Load(rm.Get() + 1);
+        }
+        });
+    thread t2([&rm]{
+        for (int i = 0; i < 1000; ++i)
+        {
+            rm.Load(rm.Get() - 1);
+        }
+        });
+
+    t1.join();
+    t2.join();
+    cout<<"rm.Get() return: "<< rm.Get() << endl;
+    // res_mgr rm1, rm2;
+    // rm1 = rm2;
     return 0;
 }
